dde98a11b0e6e3947f932fb2030aa11e736635cb
Siddharth11235 <siddharth11235@gmail.com>
2019-02-05 11:29:53 +0530

    Functional matrix linearprog routine

--- sci_gateway/cpp/libFOSSEE_Optimization_Toolbox.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LIBFOSSEE_OPTIMIZATION_TOOLBOX_GW_H__
-#define __LIBFOSSEE_OPTIMIZATION_TOOLBOX_GW_H__
-
-#include "c_gateway_prototype.h"
-
-C_GATEWAY_PROTOTYPE(sci_linearprog);
-
-#endif /* __LIBFOSSEE_OPTIMIZATION_TOOLBOX_GW_H__ */
